d3fad4204e5265302b8c54cfcc666407fe77c2a6
vera <subrakova.vera@gmail.com>
2025-05-23 15:41:54 +0700

    Add files

---
+++ scripts/03_parsing_gbk.sh
@@ -0,0 +1,13 @@
+conda init
+conda activate spacedust
+
+mkdir ./data/splitted_faa ./data/splitted_fna
+
+
+find ./data/gbk -name "*.gbk" | \
+xargs -P 2 -I {} python ./scripts/split_gbk_by_protocluster_faa.py {} ./data/splitted_faa
+
+
+find ./data/gbk -name "*.gbk" | \
+xargs -P 2 -I {} python ./scripts/split_gbk_by_protocluster_fna.py {} ./data/splitted_fna
+
